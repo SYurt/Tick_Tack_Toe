4ab409e90fa043fffe394b544b28fe034f8468b4
kuzmenko <kuzmenkosasha13@gmail.com>
2022-11-01 08:19:48 +0200

    Add newgame and demo rating

---
+++ NewGame.h
@@ -0,0 +1,4 @@
+int newGame(int player1, int player2){
+	int result = refereeFunc(player1,player2);
+	return result;	
+}
---
+++ rating.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+int main(int newGameRes){
+	newGameRes=0;
+	int textArr[1];
+    if (newGameRes==1){
+		char * ratingfile = "rating_player1.txt";
+    	FILE *fp;
+    	if((fp= fopen(ratingfile, "r"))==NULL)
+    	{
+      		perror("Error occured while opening file");
+        	return 1;
+    	}
+    	int i=0;
+		while( !feof(fp) ) {
+			fscanf(fp,"%d", &textArr[i]);
+			printf("%d ",textArr[i]);
+			i++;
+		}
+    	fclose(fp);
+    	textArr[0]++;
+    	if((fp= fopen(ratingfile, "w"))==NULL)
+    	{
+        	perror("Error occured while opening file");
+        	return 1;
+    	}
+    	fprintf(fp, "%d", textArr[0]);
+    	fclose(fp);
+	}
+	else{
+		char * ratingfile = "rating_pc.txt";
+    	FILE *fp;
+    	if((fp= fopen(ratingfile, "r"))==NULL)
+    	{
+      		perror("Error occured while opening file");
+        	return 1;
+    	}
+    	int i=0;
+		while( !feof(fp) ) {
+			fscanf(fp,"%d", &textArr[i]);
+			printf("%d ",textArr[i]);
+			i++;
+		}
+    	fclose(fp);
+    	textArr[0]++;
+    	if((fp= fopen(ratingfile, "w"))==NULL)
+    	{
+        	perror("Error occured while opening file");
+        	return 1;
+    	}
+    	fprintf(fp, "%d", textArr[0]);
+    	fclose(fp);
+	}
+return 0;
+}
